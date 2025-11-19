c791ea3cbf0a271564a6e118a3d3e9c20e6b925a
Teicross <nutthapol.tni@gmail.com>
2025-11-20 00:11:09 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/ชื่อ-repo/',
+  base: '/dot-smash-booking/',
   plugins: [react()],
 });
